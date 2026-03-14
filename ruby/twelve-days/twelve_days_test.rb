a277544197fcc8ec82c01d3359106fd6f3bfdd46
irridescentrambler <mohadikarnikhil09@gmail.com>
2026-03-14 16:33:12 +0530

    Twelve days (Ruby)

---
+++ ruby/twelve-days/twelve_days_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require_relative 'twelve_days'
+require 'pry'
+
+class TwelveDaysTest < Minitest::Test
+  def test_the_whole_song
+    song_file = File.expand_path('song.txt', __dir__)
+    expected = IO.read(song_file)
+    assert_equal expected, TwelveDays.song
+  end
+end
